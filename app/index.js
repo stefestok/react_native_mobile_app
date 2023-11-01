75202175ad607adbf3d66998e1ede2605b78a997
Štefan Eštok <stefestok@gmail.com>
2023-11-01 09:38:59 +0100

    create index.js and _layout.js with basic structure

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native-web";
+
+const Home = () => {
+    return (
+        <view>
+            <text>Home</text>
+        </view>
+    )
+}
+
+export default Home;
